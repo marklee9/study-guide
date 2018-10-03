2ce94afbc7f55f58dcbfb07d83fef0ee0477ddf0
mark <m.arklee@outlook.com>
2018-10-03 11:18:11 -0700

    Finish Vertex and Edge classes

--- Algorithms/Graph/lib/graph.rb
+++ Algorithms/Graph/lib/graph.rb
@@ -1,15 +1,36 @@
 class Vertex
-  def initialize(value)
+  attr_reader :value
+  attr_accessor :in_edges, :out_edges
 
+  def initialize(value)
+    @value = value
+    @in_edges = [];
+    @out_edges = [];
   end
 end
 
 class Edge
-  def initialize(from_vertex, to_vertex, cost = 1)
+  attr_reader :from_vertex, :to_vertex, :cost
 
+  def initialize(from_vertex, to_vertex, cost = 1)
+    @from_vertex = from_vertex
+    @to_vertex = to_vertex
+    @cost = cost
+
+    # add itself to to_vertex edges
+    to_vertex.in_edges << self
+
+    # add itself to from_vertex edges
+    from_vertex.out_edges << self    
   end
 
   def destroy!
+    # deleteing self(edge) from two vertices
+    from_vertex.out_edges.delete(self)
+    to_vertex.in_edges.delete(self)
 
+    #set its vertices to nil
+    @from_vertex = nil
+    @to_vertex = nil
   end
 end
