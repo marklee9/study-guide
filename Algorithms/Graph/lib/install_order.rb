596159714b2e7702d0d9fa13e5ddeacf2b2d99c5
mark <m.arklee@outlook.com>
2018-10-04 15:17:56 -0700

    Working on Graph

--- Algorithms/Graph/lib/install_order.rb
+++ Algorithms/Graph/lib/install_order.rb
@@ -7,8 +7,34 @@
 
 # Import any files you need to
 
-
+require 'graph'
 
 def install_order(arr)
+  order = []
 
+  arr.each do |tuple|
+    first = Vertex.new(tuple[0])
+    second = Vertex.new(typle[1])
+    
+  end
+
+
+  order
 end
+
+# def install_order(arr)
+#   graph = []
+#   max_id = 0
+#   arr.each { |el| max_id = el.max if el.max > max_id }
+#   max_id.times { |idx| graph << Vertex.new(idx + 1) }
+#   arr.each do |el|
+#     dependency = graph.select { |vertex| vertex.value == el[1] }
+#     dependency = dependency[0]
+#     package = graph.select { |vertex| vertex.value == el[0] }
+#     package = package[0]
+#     Edge.new(dependency, package)
+#   end
+#   topological_sort(graph).map { |vertex| vertex.value }
+# end
+
+
--- Algorithms/Graph/lib/topological_sort.rb
+++ Algorithms/Graph/lib/topological_sort.rb
@@ -3,5 +3,48 @@ require_relative 'graph'
 # Implementing topological sort using both Khan's and Tarian's algorithms
 
 def topological_sort(vertices)
+  queue = Queue.new
+  result = []
 
+  # getting the vertex without in_edges.
+  vertices.each { |vertex| queue << vertex if vertex.in_edges.empty? }
+
+
+  until queue.empty?
+    current = queue.shift
+
+    if current.in_edges.empty?
+      current.out_edges.each do |out_edge|
+        queue << out_edge.to_vertex
+        out_edge.destroy!
+      end
+
+      result << current
+    else
+      queue << current
+    end
+  end
+
+  result
 end
+
+# def topological_sort(graph)
+#   queue = []
+#   graph.each do |vertex|
+#     queue << vertex if vertex.in_edges.empty?
+#   end
+
+#   sorted_result = []
+#   until queue.empty?
+#     curr_vertex = queue.shift
+#     curr_vertex.out_edges.reverse_each do |edge|
+#       edge.destroy!
+#     end
+#     sorted_result << curr_vertex
+#     graph.delete(curr_vertex)
+#     graph.each do |vertex|
+#       queue << vertex if vertex.in_edges.empty? && !queue.include?(vertex)
+#     end
+#   end
+#   graph.length.zero? ? sorted_result : []
+# end
