b6857dd23cd3fd962b13f28d424b6be056912fc5
mark <m.arklee@outlook.com>
2018-12-11 10:28:06 -0800

    Remove unnecessary comments

--- Tyler McGinnis/Modern JS/app/components/App.jsx
+++ Tyler McGinnis/Modern JS/app/components/App.jsx
@@ -1,14 +1,3 @@
-// const React = require('react');
-// const ReactRouter = require('react-router-dom');
-// const Router = ReactRouter.BrowserRouter;
-// const Route = ReactRouter.Route;
-// const Switch = ReactRouter.Switch;
-// const Nav = require('./Nav');
-// const Home = require('./Home');
-// const Battle = require('./Battle');
-// const Popular = require('./Popular');
-// const Results = require('./Results');
-
 import React from 'react';
 import { BrowseRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
