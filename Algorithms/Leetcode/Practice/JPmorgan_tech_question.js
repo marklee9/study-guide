f0347232041c4eb7f808b409affc67ff0790bcee
mark <m.arklee@outlook.com>
2019-04-08 16:58:56 -0700

    Solved shortest substring question for JP

--- Algorithms/Leetcode/Practice/JPmorgan_tech_question.js
+++ Algorithms/Leetcode/Practice/JPmorgan_tech_question.js
@@ -40,3 +40,45 @@ function shortestMatching(string, dict) {
     return result;
   }
 }
+
+// Optimal solution.
+function shortestMatch(word1, words) {
+  if (word1.length < words.length) return "";
+  let map = {};
+
+  for (let i = 0; i < word1.length; i++) {
+    let ch = word1[i];
+    if (!map[ch]) map[ch] = [];
+    map[ch].push(i);
+  }
+
+  let s, e;
+  let shortest = Infinity;
+
+  while (map[words[0]].length) {
+    let start = map[words[0]].shift();
+    let end = start;
+
+    for (let i = 1; i < words.length; i++) {
+      let ch = words[i];
+
+      if (map[ch].length) {
+        while (map[ch][0] < end) {
+          map[ch].shift();
+          if (!map[ch].length) break;
+        }
+        end = map[ch][0];
+      } else {
+        break;
+      }
+    }
+
+    if (shortest > end - start) {
+      shortest = end - start;
+      e = end;
+      s = start;
+    }
+  }
+
+  return word1.substring(s, e + 1);
+}
